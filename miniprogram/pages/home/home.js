b42cce37afaa59c22b5484d240eea0a3fb6db22f
eric <liyukun_money@163.com>
2019-01-27 21:59:04 +0800

    home1.27

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -12,30 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var arr = new Array();
-    var price = 0;
-    var list = require('../../utils/allcity.js');
-    var max = 0;
-    console.log(list.result)
-    for (var i in list.result) {
-      if (list.result[i].CabinType == "Y" && list.result[i].basicCabinType == "Y") {
-        console.log(list.result[i].SinglePrice)
-        if (typeof list.result[i].SinglePrice != 'undefined') {
-          var price = parseInt(list.result[i].SinglePrice)
-          arr.push(price);
-        }
-      }
-    }
-    console.log(arr)
-    var max = Math.max.apply(Math, arr);
-      console.log(max)
-
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    var mean = sum / arr.length;
-     console.log(mean);
+    
   },
 
   /**
