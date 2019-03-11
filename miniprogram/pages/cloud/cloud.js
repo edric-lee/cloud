0ee38d5321d50ea5549fb6d16747dbbbe7485695
nono <liyukun@163.com>
2019-03-11 15:40:57 +0800

    work3.11

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -26,36 +26,71 @@ Page({
     steps.add({
       // data 字段表示需新增的 JSON 数据
       data: {
-        list: {
-          process: '离职流程',
-          numList: [{
-            title: '离职人员',
-            name: '离职申请单填写部门和姓名',
-            model:'退费凭证',
-            src: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/timg.jpg?sign=c25d8e7b4daf1d5b00791eadaf9d09c2&t=1550330559',
-          }, {
-            title: '财务部：黄丽霞（非报名员跳过此步）',
-              name: '审核报名款是否清理完毕并盖章'
-          }, {
-            title: '财务部：黄芳萍',
-              name: '审核个人借款是否清理完毕并盖章'
-          }, {
-            title: '财务部：李玉坤',
-              name: '签字审核'
-            }]
-        }, 
         // list: {
-        //   process: '退费流程',
+        //   process: '离职流程',
         //   numList: [{
-        //     title: '报名员',
-        //     name: '1、填写费用支出单\n2、打印退费凭据',
+        //     title: '离职人员',
+        //     name: '离职申请单填写部门和姓名',
+        //     model:'离职会签单',
         //     src: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/timg.jpg?sign=c25d8e7b4daf1d5b00791eadaf9d09c2&t=1550330559',
         //   }, {
-        //     title: '部门主管',
-        //     name: '签字审核'
+        //     title: '财务部：黄丽霞（非报名员跳过此步）',
+        //       name: '审核报名款是否清理完毕并盖章'
         //   }, {
-        //     title: '财务部：黄丽霞',
-        //     name: '审核退费真实性并盖章'
+        //     title: '财务部：黄芳萍',
+        //       name: '审核个人借款是否清理完毕并盖章'
+        //   }, {
+        //     title: '财务部：李玉坤',
+        //       name: '签字审核'
+        //     }]
+        // }, 
+        list: {
+          process: '退费流程',
+          numList: [{
+            title: '报名员',
+            name: '1、填写费用支出单\n2、打印退费凭据',
+            model: '退费凭证',
+            modelList: [{
+              name: '费用支出单',
+              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（退费）.png?sign=a8b642497d1a886cbe03e82541ae3206&t=1552287472'
+            }, {
+              name: '退费凭据',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/退款凭证.png?sign=3948cff782283ca97fe4a61ff937bbac&t=1552287501'
+              }, {
+                name: '储值退款凭证',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/储值退款凭证.png?sign=2e5af04d2cf741967c4408d2c6671db7&t=1552287534'
+              }]
+          }, {
+            title: '部门主管',
+            name: '签字审核'
+          }, {
+            title: '财务部：黄丽霞',
+            name: '审核退费真实性并盖章'
+          }, {
+            title: '财务部：李玉坤',
+            name: '签字审核'
+          }, {
+            title: '校长',
+            name: '签字审核'
+          }, {
+            title: '财务部：黄芳萍',
+            name: '支付退费'
+          }]
+        },
+        // 为待办事项添加一个地理位置（113°E，23°N）
+        // list: {
+        //   process: '报销流程',
+        //   numList: [{
+        //     title: '报销人员',
+        //     name: '1、整理报销附件和发票\n（具体详见报销附件）\n2、填写费用支出单',
+        //     model: '费用支出单',
+        //     src: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/timg.jpg?sign=c25d8e7b4daf1d5b00791eadaf9d09c2&t=1550330559',
+        //   }, {
+        //     title: '财务部：何振',
+        //     name: '审核报销附件、发票和填写是否规范并盖章'
+        //   }, {
+        //     title: '部门主管',
+        //     name: '同意费用支出并签字'
         //   }, {
         //     title: '财务部：李玉坤',
         //     name: '签字审核'
@@ -64,10 +99,9 @@ Page({
         //     name: '签字审核'
         //   }, {
         //     title: '财务部：黄芳萍',
-        //     name: '支付退费'
+        //     name: '支付款项'
         //   }]
         // },
-        // 为待办事项添加一个地理位置（113°E，23°N）
       },
       success(res) {
         // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
@@ -99,9 +133,9 @@ Page({
           img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/icon2.png?sign=69dea021fd251586312ef8b24586ec37&t=1550559436',
           nav: '../steps/steps'
         }, {
-          num:3,
+          num: 3,
           name: '报销附件',
-            img:'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/icon3.png?sign=7624f4735a0952c43a6f938325134c7f&t=1550559511',
+          img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/icon3.png?sign=7624f4735a0952c43a6f938325134c7f&t=1550559511',
           nav: '../reim/reim'
         }],
         // 为待办事项添加一个地理位置（113°E，23°N）
--- miniprogram/pages/steps/steps.js
+++ miniprogram/pages/steps/steps.js
@@ -5,6 +5,7 @@ Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
+    showModalStatus: false,
     basics: 0,
     // list: {
     //   process:'退费流程',
@@ -24,13 +25,13 @@ Page({
     //     }]
     // },
     num: 0,
-    scroll: 0
+    scroll: 0,
   },
   onLoad: function (options) {
     var num = wx.getStorageSync('num');
     steps.get({
       success: res => {
-        console.log(res.data[num].list)
+        // console.log(res.data[num].list.numList[0].modelList)
         var listData = res.data[num].list
         this.setData({
           list: listData
@@ -69,11 +70,12 @@ Page({
   //   })
   // }
   showModal(e) {
-    console.log(e.currentTarget.dataset.target)
+    console.log(e.target.dataset.model)
     this.setData({
-      url: e.currentTarget.dataset.target,
+      url: e.target.dataset.model,
       showModal: true,
-      flag: false
+      flag: false,
+      showModalStatus: false
     })
   },
 
@@ -82,5 +84,61 @@ Page({
       showModal: false,
       flag: true
     })
+  },
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    var model = e.target.dataset.model
+    this.util(currentStatu)
+    this.setData({
+      model:model
+    })
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例 
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画：Y轴偏移240px后(盒子高度是240px)，停
+    animation.translateY(240).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画：Y轴不偏移，停
+      animation.translateY(0).step()
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭抽屉
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示抽屉
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
   }
 });
