0c88fb13638cf26e015daa2388b29d80bf6d20a9
nono <liyukun@163.com>
2019-03-20 08:44:52 +0800

    work3.20

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -157,7 +157,7 @@ Page({
       data: {
         list: [{
           matter: '部门季度团建',
-          acce: '1、发票\n2、团建活动申请表',
+          acce: '1、发票\n2、《团建活动申请表》',
           modelList: [{
             name: '费用支出单',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单(团建).png?sign=2cc60b6136a77cc4c92f095eda2e3381&t=1552899928'
@@ -166,18 +166,63 @@ Page({
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/南宁市新东方部门团建活动申请表.png?sign=26bf94b4fe53ac455ffc4c33ec83aebb&t=1552899845'
           }]
         }, {
-            matter: '出差差旅',
-            acce: '1、发票（行程单）\n2、出差申请单\n3、交通明细表',
+          matter: '出差差旅',
+          acce: '1、发票（行程单）\n2、《出差申请单》\n3、填写《交通明细表》',
+          modelList: [{
+            name: '费用支出单',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（差旅）.png?sign=34b631da5d8af7ee7298b02239ea1941&t=1552900342'
+          }, {
+            name: '《出差申请单》',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/出差申请表.png?sign=3442d879020563458a1557f492566788&t=1552900373'
+          }, {
+            name: '填写《交通明细表》',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/差旅费及室内交通费明细表.png?sign=15b8244a5772c63c610c05cb65d5f738&t=1552900416'
+          }]
+        }, {
+          matter: '会议活动费用\n（差旅、住宿、交通、伙食、茶歇）',
+          acce: '1、发票\n2、填写《会议费用汇总表》\n3、填写《会议车费明细表》\n4、《会议签到表》',
+          modelList: [{
+            name: '费用支出单',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单(会议费).png?sign=e91f38004eb164a967ed0435937345c0&t=1552978896'
+          }, {
+            name: '会议费用汇总表',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/会议费用汇总表.png?sign=3d554a320d09417f45655c89b5167483&t=1552978922'
+          }, {
+            name: '会议车费明细表',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/会议车费明细表.png?sign=25c7cdb2be546f2a74436c9e5c811d95&t=1552979009'
+          }, {
+            name: '会议签到表',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/会议签到表.png?sign=e98049baebc3caf0d22bb2dfe29ca1eb&t=1552979055'
+          }]
+          }, {
+            matter: '物资采购',
+            acce: '1、发票\n2、采购申请\n3、打印：订单（电商采购）',
             modelList: [{
               name: '费用支出单',
-              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（差旅）.png?sign=34b631da5d8af7ee7298b02239ea1941&t=1552900342'
+              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/采购OA.png?sign=366bc1299d8b87e6df8d7a817e139561&t=1552982428'
+            },{
+              name: '采购申请--800元以上',
+              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/采购OA.png?sign=366bc1299d8b87e6df8d7a817e139561&t=1552982428'
             }, {
-                name: '出差申请单',
-                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/出差申请表.png?sign=3442d879020563458a1557f492566788&t=1552900373'
-              }, {
-                name: '交通明细表',
-                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/差旅费及室内交通费明细表.png?sign=15b8244a5772c63c610c05cb65d5f738&t=1552900416'
-              }]
+                name: '采购申请--800元以下(含800)',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/采购（纸质）.png?sign=63d3f8e7b65d69c8638fe8b8911fa24c&t=1552982402'
+            }, {
+              name: '订单',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/订单.png?sign=c215d6f80908c8ad9e4c2f394d58b4af&t=1552982454'
+            }]
+          }, {
+            matter: '内外部招待费',
+            acce: '1、发票\n2、填写：《业务招待及礼品费用明细表》\n3、填写：《差旅及市内交通费用明细表》',
+            modelList: [{
+              name: '费用支出单',
+              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（招待）.png?sign=398f03a01b749c8cfbcc1a9a019b33dc&t=1552984492'
+            },{
+              name: '业务招待及礼品费用明细表',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/业务招待及礼品费明细表.png?sign=d3dc97cbf4a2e87bd09af214a3dc1cad&t=1552983862'
+            }, {
+                name: '差旅及市内交通费用明细表',
+                img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/差旅费及室内交通费明细表.png?sign=b2bd895d0dfb3afb069341d444ba3a6a&t=1552983934'
+            }]
           }]
       },
       success(res) {
