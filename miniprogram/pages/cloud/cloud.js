d347066deb1f831f75fcdea0915c035f94b3f05f
eric <liyukun_money@163.com>
2019-07-03 13:33:33 +0800

    home7.3

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -162,22 +162,16 @@ Page({
           matter: '部门季度团建',
           acce: '1、发票\n2、《团建活动申请表》\n3、打印团建照片',
           modelList: [{
-            name: '费用支出单',
-            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单(团建).png?sign=2cc60b6136a77cc4c92f095eda2e3381&t=1552899928'
-          }, {
             name: '团建活动申请表',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/南宁市新东方部门团建活动申请表.png?sign=26bf94b4fe53ac455ffc4c33ec83aebb&t=1552899845'
-          },{
-              name: '团建照片',
-              img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/%E5%9B%A2%E5%BB%BA%E7%85%A7%E7%89%87.png?sign=16fdc06de8ef14bc6a0eb43e7b2e1e53&t=1561710343'
+          }, {
+            name: '团建照片',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/%E5%9B%A2%E5%BB%BA%E7%85%A7%E7%89%87.png?sign=16fdc06de8ef14bc6a0eb43e7b2e1e53&t=1561710343'
           }]
         }, {
           matter: '出差差旅',
           acce: '1、发票（行程单）\n2、《出差申请单》\n3、填写《交通明细表》',
           modelList: [{
-            name: '费用支出单',
-            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（差旅）.png?sign=34b631da5d8af7ee7298b02239ea1941&t=1552900342'
-          }, {
             name: '《出差申请单》',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/出差申请表.png?sign=3442d879020563458a1557f492566788&t=1552900373'
           }, {
@@ -188,9 +182,6 @@ Page({
           matter: '会议活动费用\n（差旅、住宿、交通、伙食、茶歇）',
           acce: '1、发票\n2、填写《会议费用汇总表》\n3、填写《会议车费明细表》\n4、《会议签到表》',
           modelList: [{
-            name: '费用支出单',
-            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单(会议费).png?sign=e91f38004eb164a967ed0435937345c0&t=1552978896'
-          }, {
             name: '会议费用汇总表',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/会议费用汇总表.png?sign=3d554a320d09417f45655c89b5167483&t=1552978922'
           }, {
@@ -204,9 +195,6 @@ Page({
           matter: '物资采购',
           acce: '1、发票\n2、采购申请\n3、打印：订单（电商采购）',
           modelList: [{
-            name: '费用支出单',
-            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/采购OA.png?sign=366bc1299d8b87e6df8d7a817e139561&t=1552982428'
-          }, {
             name: '采购申请--800元以上',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/采购OA.png?sign=366bc1299d8b87e6df8d7a817e139561&t=1552982428'
           }, {
@@ -220,14 +208,34 @@ Page({
           matter: '内外部招待费',
           acce: '1、发票\n2、填写：《业务招待及礼品费用明细表》\n3、填写：《差旅及市内交通费用明细表》',
           modelList: [{
-            name: '费用支出单',
-            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/费用支出单（招待）.png?sign=398f03a01b749c8cfbcc1a9a019b33dc&t=1552984492'
-          }, {
             name: '业务招待及礼品费用明细表',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/业务招待及礼品费明细表.png?sign=d3dc97cbf4a2e87bd09af214a3dc1cad&t=1552983862'
           }, {
             name: '差旅及市内交通费用明细表',
             img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/差旅费及室内交通费明细表.png?sign=b2bd895d0dfb3afb069341d444ba3a6a&t=1552983934'
+          }]
+        }, {
+          matter: '市场活动兼职劳务',
+          acce: '1、劳务费用发放明细表\n2、活动照片',
+          modelList: [{
+            name: '劳务费用发放明细表',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/劳务费用发放明细表.png?sign=c4fb598f806d3b7c93d0bc634797ed91&t=1561965937'
+          }, {
+            name: '活动照片',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/活动照片.png?sign=6eec3710963891acd7348130288879a8&t=1561965896'
+          }]
+        }, {
+          matter: '教学奖金',
+          acce: '1、奖金汇总表\n2、奖金审批邮件',
+          modelList: [{
+            name: '奖金汇总表',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/奖金汇总表.png?sign=704e45ad152d1cdd9d2732caec2892ca&t=1561966410'
+          }, {
+            name: '奖金审批邮件-1',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/奖金申请OA(01).png?sign=3b73c05e60dee3cb762ecbe73fcd391f&t=1561966453'
+          }, {
+            name: '奖金审批邮件-2',
+            img: 'https://6669-finance-059fa2-1258372440.tcb.qcloud.la/images/SOP/奖金申请OA(02).png?sign=ecbe1a6e84bd8949e44becb2bf8e2970&t=1561966502'
           }]
         }]
       },
@@ -241,16 +249,186 @@ Page({
     costClass.add({
       data: {
         list: [{
-          matter: '活动场地租赁',
-          category: '市场推广费',
-          detailed: '市场活动费',
-          type: '活动执行费',
-        }, {
-          matter: '活动产生的相关租赁，餐饮费用',
-          category: '市场推广费',
-          detailed: '市场活动费',
-          type: '其他市场活动',
-        }]
+            matter: '活动场地租赁',
+            category: '市场推广',
+            detailed: '市场活动',
+            type: '活动执行',
+          },
+          {
+            matter: '活动产生的相关租赁，餐饮费用',
+            category: '市场推广',
+            detailed: '市场活动',
+            type: '其他市场活动',
+          },
+          {
+            matter: '市场劳务费',
+            category: '市场推广',
+            detailed: '市场活动',
+            type: '活动劳务',
+          },
+          {
+            matter: '活动差旅费',
+            category: '市场推广',
+            detailed: '市场活动',
+            type: '活动差旅',
+          },
+          {
+            matter: '朋友圈广告及订阅号（微博）年审服务费',
+            category: '市场推广',
+            detailed: '媒体推广',
+            type: '网络广告',
+          },
+          {
+            matter: '广告更换画面费',
+            category: '市场推广',
+            detailed: '媒体推广',
+            type: '广告更换画面',
+          },
+          {
+            matter: '短期户外广告（季度）',
+            category: '市场推广',
+            detailed: '媒体推广',
+            type: '户外广告',
+          },
+          {
+            matter: '人力核算确认无误的各类奖金',
+            category: '人力资源',
+            detailed: '工资劳务',
+            type: '奖金',
+          },
+          {
+            matter: '教学部门培训差旅及教研',
+            category: '人力资源',
+            detailed: '培训',
+            type: '培训',
+          },
+          {
+            matter: '各部门每Q部门团建费用',
+            category: '人力资源',
+            detailed: '团队建设',
+            type: '团队建设',
+          },
+          {
+            matter: '人力组织相关活动费用报销',
+            category: '人力资源',
+            detailed: '团队建设',
+            type: '团队建设',
+          },
+          {
+            matter: '人力组织异地定点招聘差旅',
+            category: '人力资源',
+            detailed: '招聘',
+            type: '差旅',
+          },
+          {
+            matter: '人力充值招聘账户费用',
+            category: '人力资源',
+            detailed: '招聘',
+            type: '其他招聘',
+          },
+          {
+            matter: '人力报销用于招聘广告费用',
+            category: '人力资源',
+            detailed: '招聘',
+            type: '广告',
+          },
+          {
+            matter: '资产部支付物业费',
+            category: '行政后勤',
+            detailed: '物业租赁',
+            type: '物业',
+          },
+          {
+            matter: '资产部支付水电费',
+            category: '行政后勤',
+            detailed: '物业租赁',
+            type: '水电',
+          },
+          {
+            matter: '资产部支付垃圾费',
+            category: '行政后勤',
+            detailed: '物业租赁',
+            type: '保洁',
+          },
+          {
+            matter: '校办报电信宽带费用',
+            category: '日常运营',
+            detailed: '通讯',
+            type: '通讯',
+          },
+          {
+            matter: '市内打车费',
+            category: '日常运营',
+            detailed: '市内交通',
+            type: '市内交通',
+          },
+          {
+            matter: '资产部报校车停车费和过路费，贴车膜或车辆维修费用',
+            category: '日常运营',
+            detailed: '车辆使用',
+            type: '车辆',
+          },
+          {
+            matter: '通过人力批准的差旅报销',
+            category: '日常运营',
+            detailed: '差旅',
+            type: '差旅',
+          },
+          {
+            matter: '举办或参加会议产生的相关费用',
+            category: '日常运营',
+            detailed: '会议',
+            type: '会议',
+          },
+          {
+            matter: '资产部报梦雅或锋晟印刷品费用1/2',
+            category: '日常运营',
+            detailed: '印刷制作',
+            type: '印刷制作',
+          },
+          {
+            matter: '资产部报梦雅或锋晟印刷品费用2/2',
+            category: '日常运营',
+            detailed: '印刷制作',
+            type: '印刷市场',
+          },
+          {
+            matter: '部门产生业务招待相关餐饮或礼品费',
+            category: '日常运营',
+            detailed: '招待及礼品',
+            type: '招待礼品',
+          },
+          {
+            matter: '校办报打印印刷费',
+            category: '日常运营',
+            detailed: '其他日常',
+            type: '打印印刷用（校办）',
+          },
+          {
+            matter: '校办报打印机租金',
+            category: '日常运营',
+            detailed: '其他日常',
+            type: '打印机租金',
+          },
+          {
+            matter: '邮寄产生费用',
+            category: '日常运营',
+            detailed: '其他日常',
+            type: '邮寄',
+          },
+          {
+            matter: '资产部采购各部门低值易耗品',
+            category: '物资采购',
+            detailed: '办公用品/耗材采购',
+            type: '低值易耗品',
+          },
+          {
+            matter: '资产部采购各部门办公用品',
+            category: '物资采购',
+            detailed: '办公用品/耗材采购',
+            type: '办公用品',
+          },
+        ]
       },
       success(res) {
         console.log(res)
@@ -262,14 +440,81 @@ Page({
     payClass.add({
       data: {
         list: [{
-          matter: '一管借款或采购专员采购借款',
-          detailed: '申请借款',
-          type: '员工个人借款（短期使用）',
-        }, {
-          matter: '前台备用金',
-          detailed: '申请借款',
-          type: '员工公务备用（长期使用）',
-        }]
+            matter: '一管借款或采购专员采购借款',
+            detailed: '申请借款',
+            type: '员工个人借款（短期使用）',
+          },
+          {
+            matter: '前台备用金',
+            detailed: '申请借款',
+            type: '员工公务备用（长期使用）',
+          },
+          {
+            matter: '资产部报销支付外部供应商教材款',
+            detailed: '申请付款',
+            type: '教材款（外部供应商）',
+          },
+          {
+            matter: '资产部报销支付内部供应商教材款',
+            detailed: '申请付款',
+            type: '教材款（集团内部供应商）',
+          },
+          {
+            matter: '资产部报销支付装修工程款',
+            detailed: '申请付款',
+            type: '装修工程款',
+          },
+          {
+            matter: '资产部报销支付房屋押金',
+            detailed: '申请付款',
+            type: '房屋押金',
+          },
+          {
+            matter: '资产部报销支付房屋质保金',
+            detailed: '申请付款',
+            type: '质保金',
+          },
+          {
+            matter: '资产部报销已知分摊比例校区1/2',
+            detailed: '申请付款',
+            type: '校区租金（1/2办公房租）',
+          },
+          {
+            matter: '资产部报销已知分摊比例校区2/2',
+            detailed: '申请付款',
+            type: '校区租金（2/2教室房租）',
+          },
+          {
+            matter: '资产部报销未知分摊比例校区',
+            detailed: '申请付款',
+            type: '校区租金（长期待摊费用）',
+          },
+          {
+            matter: '资产部报销预付广告费用',
+            detailed: '申请付款',
+            type: '预付广告费',
+          },
+          {
+            matter: '资产部报销预付饮水机租金',
+            detailed: '申请付款',
+            type: '预付饮水机租赁费',
+          },
+          {
+            matter: '资产部报销预付物业费',
+            detailed: '申请付款',
+            type: '预付物业费',
+          },
+          {
+            matter: '资产部报销预付校区保洁费',
+            detailed: '申请付款',
+            type: '预付校区保洁费',
+          },
+          {
+            matter: '资产部报销预付宽带网络',
+            detailed: '申请付款',
+            type: '预付宽带网络费',
+          },
+        ]
       },
       success(res) {
         console.log(res)
